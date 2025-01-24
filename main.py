dc8c10fcdb09c8d4502bb1dca012213c8f437382
ericgr3gory <eric@e-gregory.com>
2025-01-24 09:33:45 -0800

    cahnge function name from ping_host to is_host_up change ping to udp

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def current_log_name():
     log_name = f'/home/ericgr3gory/mmx-node/mainnet/logs/mmx_harvester_{current_date.year}_{current_date.month:02}_{current_date.day:02}.txt'
     return log_name
 
-def ping_host(host):
-    response = ping(host)
+def is_node_host_up(host):
+    response = ping(host, udp=True)
     if response:   
         notification(f'{host} is up')     
     else:
@@ -31,8 +31,8 @@ def main():
         if 'WARN:' in line:
             print(line)
             notification(line)
-            ping_host(os.getenv('NODE_HOST'))
-        time.sleep(30)
+            is_node_host_up(os.getenv('NODE_HOST'))
+        time.sleep(10)
     
 
 if __name__ == "__main__":
