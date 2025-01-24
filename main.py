bdd70f6d1db451df51528a99bb85cd43bc537ee7
ericgr3gory <eric@e-gregory.com>
2025-01-24 10:34:43 -0800

    moved path to log dir to .env

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def last_line_of_log(filename):
 
 def current_log_name():
     current_date = date.today()
-    log_name = f'/home/ericgr3gory/mmx-node/mainnet/logs/mmx_harvester_{current_date.year}_{current_date.month:02}_{current_date.day:02}.txt'
+    log_name = f'mmx_harvester_{current_date.year}_{current_date.month:02}_{current_date.day:02}.txt'
     return log_name
 
 def is_node_host_up(host):
@@ -27,12 +27,10 @@ def is_node_host_up(host):
         
 def main():
     while True:
-        file = current_log_name()
-        line = last_line_of_log(file)
+        log_file = current_log_name()
+        line = last_line_of_log(f'{os.getenv('LOG_DIR')}{log_file}')
         if 'WARN:' in line:
-            print(line)
             notification(line)
-            print(f"node = {os.getenv('NODE_HOST')}")
             is_node_host_up(os.getenv('NODE_HOST'))
         time.sleep(10)
     
